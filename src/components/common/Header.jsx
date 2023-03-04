8adf90e2a21949105b8372a5b967398f31fb7455
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-04 18:34:26 -0500

    Moves header to common components folder

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,24 @@
+import {
+  HomeIcon,
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  QuestionMarkCircleIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
+
+export default function Header({ setOpen, open }) {
+  const handleCick = () => setOpen(!open);
+  return (
+    <header className="bg-primary nav-height w-full flex justify-between px-5 box-border">
+      <div className="flex items-center">
+        <Bars3Icon className="top-nav-icon" onClick={handleCick} />
+        <HomeIcon className="top-nav-icon" />
+        <MagnifyingGlassIcon className="top-nav-icon" />
+      </div>
+      <div className=" flex items-center">
+        <QuestionMarkCircleIcon className="top-nav-icon" />
+        <UserCircleIcon className="top-nav-icon" />
+      </div>
+    </header>
+  );
+}
