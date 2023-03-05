a615744a1dbce640b2b379f993de06c4f995ca85
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-05 10:25:12 -0500

    Ensures sidebar dissapears fully on close

--- src/components/common/SideNav.jsx
+++ src/components/common/SideNav.jsx
@@ -14,29 +14,33 @@ export default function SideNav({ open }) {
           open ? "w-60" : "w-0"
         } bg-secondary fixed left-0 bottom-0 shadow-sm`}
       >
-        <div className="flex flex-col w-full px-4 pt-9 space-y-4">
+        <div
+          className={`flex flex-col w-full px-4 pt-9 space-y-4 ${
+            !open && "opacity-0 translate-x-12"
+          }`}
+        >
           <Link to="/">
-            <div className="duration-300 relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
+            <div className="text-primary duration-300 relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
               <MapIcon className="w-6" />
-              <p className={`${!open && "opacity-0"}`}>Dashboard</p>
-            </div>
-          </Link>
-          <Link to="/inbox">
-            <div className="duration-300 relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
-              <InboxIcon className="w-6" />
-              <p className={`${!open && "opacity-0"}`}>Inbox</p>
+              <p>Dashboard</p>
             </div>
           </Link>
           <Link to="/today">
-            <div className="duration-300 relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
+            <div className="text-primary duration-300 relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
               <SunIcon className="w-6" />
-              <p className={`${!open && "opacity-0"}`}>Today</p>
+              <p>Today</p>
+            </div>
+          </Link>
+          <Link to="/inbox">
+            <div className="text-primary duration-300 relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
+              <InboxIcon className="w-6" />
+              <p>Inbox</p>
             </div>
           </Link>
           <Link to="/upcoming">
-            <div className="duration-300  relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
+            <div className="text-primary duration-300  relative h-8 flex items-center space-x-2 cursor-pointer rounded-md hover:bg-primary hover:text-white pl-2">
               <CalendarDaysIcon className="w-6" />
-              <p className={`${!open && "opacity-0"}`}>Upcoming</p>
+              <p>Upcoming</p>
             </div>
           </Link>
         </div>
