6b250314778dd5e909e68e334418a860f05bb2ce
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-05 10:28:17 -0500

    Makes page responsive

--- src/components/common/PageWrapper.jsx
+++ src/components/common/PageWrapper.jsx
@@ -3,8 +3,11 @@ export default function PageWrapper({ open, children }) {
     <>
       <div
         className={`side-nav-height duration-500 ${
-          open ? "main-width" : "w-full"
-        } fixed right-0 bottom-0 shadow-sm`}
+          open ? "sm:main-width" : "w-full"
+        }
+        ${
+          open ? "pl-4" : "pl-14"
+        } fixed right-0 bottom-0 shadow-sm pr-14 p-l -z-10`}
       >
         {children}
       </div>
