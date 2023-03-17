94468a009030a1b0f5c56e55d030852ffe022009
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-17 14:19:39 -0400

    Adds daisyUI avatar component

--- src/components/layout/StatsBar/Statsbar.jsx
+++ src/components/layout/StatsBar/Statsbar.jsx
@@ -8,34 +8,40 @@ export default function Statsbar() {
 
   return (
     <div
-      className={`mb-5 flex rounded-b-lg bg-secondary px-3 py-2 ${
-        statsOpen ? "h-40" : "h-10"
+      className={`mb-5 flex rounded-b-lg bg-secondary px-3 py-2 duration-200 ease-in ${
+        statsOpen ? "h-44" : "h-10"
       }`}
     >
-      <div>
-        <img
-          src={Mario}
-          alt={"Ari's Profile"}
-          className={`mx-auto mb-2 h-28 w-28 rounded-full ${
-            !statsOpen && "hidden"
-          }`}
-        />
-        <div className="flex self-end">
-          <h1 className="whitespace-pre font-bold text-primary">Ari-the-don</h1>
-          <h1 className="whitespace-pre pl-2 text-primary opacity-75">
-            · Level 1
-          </h1>
+      <div className="flex flex-col">
+        <div
+          className={`${
+            !statsOpen && "-translate-y-32"
+          } mx-auto duration-200 ease-in`}
+        >
+          <div className="avatar left-8">
+            <div className="my-2 w-24 rounded-full">
+              <img src={Mario} alt={"Ari's Profile"} />
+            </div>
+          </div>
+          <div className="flex self-end">
+            <h1 className="whitespace-pre font-bold text-primary">
+              Ari-the-don
+            </h1>
+            <h1 className="whitespace-pre pl-2 text-primary opacity-75">
+              · Level 1
+            </h1>
+          </div>
         </div>
       </div>
       <StatsSection statsOpen={statsOpen} />
       {statsOpen ? (
         <ChevronUpIcon
-          className="h-10 w-10 self-end"
+          className="h-10 w-10 self-end hover:cursor-pointer"
           onClick={() => setStatsOpen(!statsOpen)}
         />
       ) : (
         <ChevronDownIcon
-          className="h-10 w-10 self-end"
+          className="h-10 w-10 self-end hover:cursor-pointer"
           onClick={() => setStatsOpen(!statsOpen)}
         />
       )}
