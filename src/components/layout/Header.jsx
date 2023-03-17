530c266678984f733e4a1d03544256e0ae41457d
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-16 22:15:04 -0400

    Replaces header with daisyUI navbar

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,24 +4,99 @@ import {
   MagnifyingGlassIcon,
   QuestionMarkCircleIcon,
   UserCircleIcon,
-} from "@heroicons/react/24/solid";
-import { Link } from "react-router-dom";
+} from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
+import profile from '../../assets/images/Mario.jpeg';
 
 export default function Header({ setIsSideNavOpen, isSideNavOpen }) {
   const handleCick = () => setIsSideNavOpen(!isSideNavOpen);
   return (
-    <header className="fixed top-0 box-border flex h-11 w-full justify-between bg-primary px-5">
-      <div className="flex items-center">
-        <Bars3Icon className="top-nav-icon" onClick={handleCick} />
-        <Link to="/">
-          <HomeIcon className="top-nav-icon" />
-        </Link>
-        <MagnifyingGlassIcon className="top-nav-icon" />
+    <>
+      <div className="navbar fixed z-10 bg-primary text-primary-content">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div onClick={handleCick} className="btn-ghost btn-circle btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <a className="btn-ghost btn text-xl normal-case">HaDventure</a>
+        </div>
+        <div className="navbar-end">
+          <button className="btn-ghost btn-circle btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <button className="btn-ghost btn-circle btn">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge-primary badge badge-xs indicator-item"></span>
+            </div>
+          </button>
+          <div className="flex-none">
+            <div className="dropdown-end dropdown">
+              <label tabindex="0" class="btn-ghost btn-circle avatar btn">
+                <div class="w-10 rounded-full">
+                  <img src={profile} />
+                </div>
+              </label>
+              <ul
+                tabindex="0"
+                class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 text-primary shadow"
+              >
+                <li>
+                  <a class="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className=" flex items-center">
-        <QuestionMarkCircleIcon className="top-nav-icon" />
-        <UserCircleIcon className="top-nav-icon" />
-      </div>
-    </header>
+    </>
   );
 }
