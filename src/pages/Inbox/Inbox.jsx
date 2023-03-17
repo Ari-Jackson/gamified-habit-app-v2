7c3df1ca6b2bea610bab7874df82333dc90b606a
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-17 14:06:49 -0400

    Adds begining content to Incoming/Inbox page

--- src/pages/Inbox/Inbox.jsx
+++ src/pages/Inbox/Inbox.jsx
@@ -1,7 +1,18 @@
+import Task from "../../components/common/TodoComponent/Task";
+import taskData from "../../../database/db.json";
 export default function Inbox() {
   return (
     <>
-      <div>Incoming</div>
+      <header>
+        <div className="pt-5">
+          <h1 className="text-2xl font-bold text-primary">Incoming</h1>
+        </div>
+      </header>
+      <ul>
+        {taskData.tasks.map((task) => {
+          return <Task key={task.id} taskData={task} />;
+        })}
+      </ul>
     </>
   );
 }
