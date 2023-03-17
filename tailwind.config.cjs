6fc8c2376c60e13c263f96425cf763c57b346628
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-16 22:13:41 -0400

    Adds my daisyui theme to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../index.html", "./*.{js,jsx}", "./**/*.{js,jsx}"],
+  content: ['../index.html', './*.{js,jsx}', './**/*.{js,jsx}'],
   theme: {
     extend: {
-      colors: {
-        primary: "#3d3d3d",
-        secondary: "#e1e1e1",
-      },
+      colors: {},
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        hadventuretheme: {
+          primary: '#3d3d3d',
+          secondary: '#e1e1e1',
+          accent: '#37cdbe',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+    ],
+    logs: true,
+    darkTheme: 'dark',
+  },
 };
