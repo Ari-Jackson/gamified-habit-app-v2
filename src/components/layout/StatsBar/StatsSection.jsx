a811d7269e3bd42fed99284607511f2286bbc5fe
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-17 14:05:01 -0400

    Replaces stats section componets with daisyUI

--- src/components/layout/StatsBar/StatsSection.jsx
+++ src/components/layout/StatsBar/StatsSection.jsx
@@ -5,38 +5,80 @@ import {
 } from "@heroicons/react/24/solid";
 
 export default function StatsSection({ statsOpen }) {
+  const respondToState = () => !statsOpen && "-translate-y-6";
+  const setVisibility = () => !statsOpen && "hidden";
+  const setFormat = () =>
+    !statsOpen && "flex flex-row-reverse items-center pb-1";
   return (
-    <div className="flex flex-grow flex-col justify-center gap-9 px-4">
-      <div className={`w-80 ${!statsOpen && "hidden"}`}>
+    <div
+      className={
+        "flex flex-grow flex-col gap-4 px-4 duration-200 ease-in " +
+        respondToState()
+      }
+    >
+      <div className={`w-80 duration-200 ${setVisibility()}`}>
         <div className="flex">
           <h1 className="flex-grow">XP</h1>
           <h1>3000</h1>
         </div>
-        <div className="absolute  z-0 h-5 w-80 rounded-xl bg-primary"></div>
-        <div className="absolute z-10 h-5 w-40 rounded-xl bg-amber-300"></div>
+        <progress
+          className="progress progress-warning h-4 w-80"
+          value="10"
+          max="100"
+        ></progress>
       </div>
-      <div
-        className={`${statsOpen ? "h-12" : "h-7"} flex w-80 justify-between`}
-      >
-        <div className="flex">
-          <CircleStackIcon className="w-12 text-yellow-500" />
-          <div className="-mx-4 my-auto flex h-full w-12 items-center justify-center rounded-full bg-primary">
-            <span className="text-white">365</span>
+      <div className="w-fit duration-200 ease-in">
+        <div class="stats h-full w-full overflow-visible shadow">
+          <div class={`stat duration-200 ease-in ${setFormat()}`}>
+            <div class="stat-figure text-secondary">
+              <CircleStackIcon className="w-8 text-yellow-500" />
+            </div>
+            <div class={`stat-title duration-200 ${setVisibility()}`}>Gold</div>
+            <div class="stat-value text-lg">840</div>
           </div>
-        </div>
-        <div className="flex">
-          <HeartIcon className="w-12 text-rose-600" />
-          <div className="-mx-4 my-auto flex h-full w-12 items-center justify-center rounded-full bg-primary">
-            <span className="text-white">12</span>
+          <div class={`stat duration-200 ${setFormat()}`}>
+            <div class="stat-figure text-secondary">
+              <HeartIcon className="w-8 text-rose-600" />
+            </div>
+            <div class={`stat-title duration-200 ${setVisibility()}`}>
+              Heart
+            </div>
+            <div class="stat-value text-lg">12</div>
           </div>
-        </div>
-        <div className="flex">
-          <SparklesIcon className="w-12 text-cyan-600" />
-          <div className="-mx-4 my-auto flex h-full w-12 items-center justify-center rounded-full bg-primary">
-            <span className="text-white">4</span>
+          <div class={`stat duration-200 ${setFormat()}`}>
+            <div class="stat-figure text-secondary">
+              <SparklesIcon className="w-8 text-cyan-600" />
+            </div>
+            <div class={`stat-title duration-200 ${setVisibility()}`}>Mana</div>
+            <div class="stat-value text-lg">50</div>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+// function newFunc() {
+//   return (
+//     <>
+//       <div className="flex">
+//         <CircleStackIcon className="w-12 text-yellow-500" />
+//         <div className="-mx-4 my-auto flex h-full w-12 items-center justify-center rounded-full bg-primary">
+//           <span className="text-white">365</span>
+//         </div>
+//       </div>
+//       <div className="flex">
+//         <HeartIcon className="w-12 text-rose-600" />
+//         <div className="-mx-4 my-auto flex h-full w-12 items-center justify-center rounded-full bg-primary">
+//           <span className="text-white">12</span>
+//         </div>
+//       </div>
+//       <div className="flex">
+//         <SparklesIcon className="w-12 text-cyan-600" />
+//         <div className="-mx-4 my-auto flex h-full w-12 items-center justify-center rounded-full bg-primary">
+//           <span className="text-white">4</span>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
