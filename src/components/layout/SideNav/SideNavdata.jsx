952601f87c69831b72f15329e336cb18bd717e76
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-18 14:12:26 -0400

    Slims down app

--- src/components/layout/SideNav/SideNavdata.jsx
+++ src/components/layout/SideNav/SideNavdata.jsx
@@ -8,15 +8,9 @@ import {
 
 export const sideNavData = [
   {
-    title: "Dashboard",
-    icon: () => <MapIcon className="w-6" />,
-    link: "/",
-    sectionEnd: false,
-  },
-  {
     title: "Today",
     icon: () => <SunIcon className="w-6" />,
-    link: "/today",
+    link: "/",
     sectionEnd: true,
   },
   {
@@ -24,17 +18,5 @@ export const sideNavData = [
     icon: () => <InboxIcon className="w-6" />,
     link: "/incoming",
     sectionEnd: false,
-  },
-  {
-    title: "Missions",
-    icon: () => <CalendarDaysIcon className="w-6" />,
-    link: "/missions",
-    sectionEnd: true,
-  },
-  {
-    title: "Training Center",
-    icon: () => <TrophyIcon className="w-6" />,
-    link: "/training-center",
-    sectionEnd: true,
   },
 ];
