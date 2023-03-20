ad303039748380b84f9c4b3b1edbbed11887982d
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-20 11:18:03 -0400

    Renames and moves TaskModal and Header

--- src/components/common/TaskModal/TaskModal.jsx
+++ src/components/common/TaskModal/TaskModal.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { useNavigate, useParams } from "react-router-dom";
-import TaskCheckMark from "../TaskComponents/TaskCheckMark";
-import TaskModalHeader from "./TaskModalComponents/TaskModalHeader";
+import ModalHeader from "./TaskModalComponents/ModalHeader";
+import ModalMainContent from "./TaskModalComponents/ModalMainContent";
+import ModalEditSection from "./TaskModalComponents/ModalEditSection";
 
 export default function TaskModal({ tasks }) {
   const [isOpen, setIsOpen] = useState(true);
@@ -18,24 +19,21 @@ export default function TaskModal({ tasks }) {
     const chosenTask = tasks.find((task) => {
       return task.id === Number(taskIdString);
     });
-
-    const { taskTitle, description, tags, difficulty, priority, project } =
-      chosenTask;
+    const { taskTitle, description, project } = chosenTask;
 
     return (
       <Dialog open={isOpen} onClose={handleClose} className="relative z-50">
         <div className="fixed inset-0 flex items-center justify-center bg-slate-900 bg-opacity-40 p-8">
           <Dialog.Panel className="flex h-full w-full max-w-4xl flex-col rounded-lg bg-white">
-            <TaskModalHeader project={project} handleClose={handleClose} />
+            <ModalHeader project={project} handleClose={handleClose} />
             <hr />
             <article className="flex h-full flex-col gap-4 overflow-y-auto p-4">
-              <div className="flex p-4">
-                <TaskCheckMark />
-                <div className="">
-                  <Dialog.Title> {taskTitle}</Dialog.Title>
-                  <p>{description}</p>
-                </div>
-              </div>
+              <ModalMainContent description={description}>
+                <Dialog.Title className="flex text-xl font-semibold">
+                  {taskTitle}
+                </Dialog.Title>
+              </ModalMainContent>
+              <ModalEditSection chosenTask={chosenTask} />
             </article>
           </Dialog.Panel>
         </div>
--- src/components/common/TaskModal/TaskModalComponents/ModalHeader.jsx
+++ src/components/common/TaskModal/TaskModalComponents/ModalHeader.jsx
@@ -1,7 +1,7 @@
 import { TfiClose } from "react-icons/tfi";
-export default function TaskModalHeader({ project, handleClose }) {
+export default function ModalHeader({ project, handleClose }) {
   return (
-    <header className="py-1leading-10 flex items-center gap-4  rounded-t-lg px-4">
+    <header className="flex items-center gap-4 rounded-t-lg py-1  px-4 leading-10">
       <div className="flex-grow">
         <h3>{project}</h3>
       </div>
